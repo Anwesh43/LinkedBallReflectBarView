e8eaa839143dc82e495d42ff8b70c3f97bf4ef23
Anwesh43 <anweshthecool0@gmail.com>
2021-04-07 01:04:28 +0530

    adding Canvas extension methods

--- BallReflectBarView/src/main/java/com/example/ballreflectbarview/BallReflectBarView.kt
+++ BallReflectBarView/src/main/java/com/example/ballreflectbarview/BallReflectBarView.kt
@@ -30,3 +30,26 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBallReflectBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = Math.min(w, h) / ballRFactor
+    val sf1 : Float = scale.divideScale(0, parts)
+    val sf2 : Float = scale.divideScale(1, parts)
+    val sf3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    drawCircle(
+        -w / 2 + r + (w - 2 * r) * sf2,
+        -h / 2 + r + (h - 2 * r) * sf2.sinify(),
+        r * (sf1 - sf3),
+        paint
+    )
+    restore()
+}
+
+fun Canvas.drawBRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBallReflectBar(scale, w, h, paint)
+}
